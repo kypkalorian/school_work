a391ef02b68807edbd672a6929321b1e6f1c548b
Colin Baxter <colin.baxter13@gmail.com>
2017-01-24 10:34:22 -0700

    commit now

---
+++ 3260_folder/classwork/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classWork3260_1_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("classwork 3260");
+            Console.ReadLine();
+        }
+    }
+}
